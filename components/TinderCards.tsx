fe5db4e9a7665ea00db405d6c147e796f0b2507d
Anuarbekov <mikonek.a@gmail.com>
2023-04-11 21:32:15 +0600

    nothing new

--- components/TinderCards.tsx
+++ components/TinderCards.tsx
@@ -3,7 +3,7 @@ import TinderCard from "react-tinder-card";
 import { v4 as uuidv4 } from "uuid";
 
 interface TinderCardsProps {
-  urls: string[];
+  urls: any;
   collection_id: string;
   resolution: string;
 }
@@ -13,7 +13,7 @@ const TinderCards: React.FC<TinderCardsProps> = ({
   collection_id,
   resolution,
 }) => {
-  const onSwipe = async (direction: string, url: string) => {
+  const onSwipe = (direction: string, url: string) => {
     const data = {
       url_of_photo: url,
       reaction: direction === "right" ? "+" : "-",
@@ -21,22 +21,19 @@ const TinderCards: React.FC<TinderCardsProps> = ({
   };
   return (
     <>
-      {urls.map((url) => (
-        <TinderCard
-          onSwipe={(direction) => onSwipe(direction, url)}
-          key={uuidv4()}
-          preventSwipe={["up", "down"]}
-          className="swipe"
-        >
-          <img
-            alt=""
-            className={`image-slider-${
-              resolution === "square" ? "square" : "vertical"
-            }`}
-            src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80"
-          />
+      <TinderCard
+        onSwipe={(direction) => onSwipe(direction, urls[0])}
+        preventSwipe={["up", "down"]}
+        className="swipe"
+      >
+        <img
+          alt=""
+          className={`image-slider-${
+            resolution === "square" ? "square" : "vertical"
+          }`}
+          src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80"
+        />
         </TinderCard>
-      ))}
     </>
   );
 };
